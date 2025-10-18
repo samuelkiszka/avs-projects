57ac248b50f255e9b96c9cbb73d014519359270d
Samuel Kiszka <samuel.kiszka26@gmail.com>
2025-10-18 16:50:17 +0200

    feat: Reflect half of the set

--- proj1/calculators/LineMandelCalculator.cc
+++ proj1/calculators/LineMandelCalculator.cc
@@ -18,15 +18,58 @@
 LineMandelCalculator::LineMandelCalculator (unsigned matrixBaseSize, unsigned limit) :
     BaseMandelCalculator(matrixBaseSize, limit, "LineMandelCalculator")
 {
-    // @TODO allocate & prefill memory
+    data = (int *)(malloc(height * width * sizeof(int)));
 }
 
 LineMandelCalculator::~LineMandelCalculator() {
-    // @TODO cleanup the memory
+    free(data);
+    data = NULL;
+}
+
+template <typename T>
+static inline int mandelbrot(T real, T imag, int limit)
+{
+    T zReal = real;
+    T zImag = imag;
+
+    for (int i = 0; i < limit; ++i)
+    {
+        T r2 = zReal * zReal;
+        T i2 = zImag * zImag;
+
+        if (r2 + i2 > 4.0f)
+            return i;
+
+        zImag = 2.0f * zReal * zImag + imag;
+        zReal = r2 - i2 + real;
+    }
+    return limit;
 }
 
 
 int * LineMandelCalculator::calculateMandelbrot () {
-    // @TODO implement the calculator & return array of integers
-    return NULL;
+    int *pdata = data;
+    float ii = 0.0f;
+
+    for (int i = 0; i < height / 2; i++, ii++)
+    {
+        float jj = 0.0f;
+        for (int j = 0; j < width; j++, jj++)
+        {
+            float x = x_start + jj * dx; // current real value
+            float y = y_start + ii * dy; // current imaginary value
+
+            int value = mandelbrot(x, y, limit);
+
+            *(pdata++) = value;
+        }
+    }
+
+    int line = height / 2;
+    while(line--) {
+        for (int line_pos = 0; line_pos < width; line_pos++)
+            *(pdata++) = *(data + line * width + line_pos);
+    }
+
+    return data;
 }
